5ac8791889d6d66b2edeb29cd1de5a675c921056
xyezir <xyezir@gmail.com>
2016-11-02 17:08:59 +0800

    remove webpack require

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 "use strict";
 require("any-promise/register")("bluebird");
 
-
 const logger = require("debug")("leaf:mw:react");
 const react = require('koa-react-view');
 const register = require('babel-register');
@@ -9,8 +8,6 @@ const es2015 = require('babel-preset-es2015');
 const reactPreset = require('babel-preset-react');
 const pathModule = require('path');
 const extend = require('extend');
-
-const webpack = require('webpack')
 
 const DEFAULTS = {};
 var options;
@@ -25,9 +22,6 @@ class middleware {
 
     let leaf = this;
     let koa = this.koa;
-
-    let webpackConfig = {};
-    logger('Enable webpack dev and HMR middleware')
 
     const viewDir = pathModule.resolve(leaf.basepath, "app/View");
 
